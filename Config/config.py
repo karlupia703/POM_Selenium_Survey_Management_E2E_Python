da478dbd3b83631ea5640cf57ad75478a0069015
karlupiarajni <rajni.1156@zenmonk.tech>
2025-05-26 16:02:27 +0530

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -2,5 +2,5 @@ class Config:
     base_url = "http://localhost:3000/"
     language = "Español"  # Options: "Español", "en_US" , pt_BR
 
-    Email = "zenmonk-developer-zenmonk@zenmonk.tech"   # zenmonk-developer-zenmonk@zenmonk.tech
-    Password = "a2fiEz5Sx2LzRiJ"    # "a2fiEz5Sx2LzRiJ"
+    Email = "user@gmail.com"
+    Password = "password"
