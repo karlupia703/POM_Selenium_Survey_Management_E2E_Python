386167aa5ea34a04d958a13ee262f601b5e547e1
karlupiarajni <rajni.1156@zenmonk.tech>
2025-05-26 16:00:37 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,5 +2,5 @@ class Config:
     base_url = "https://survey-building-app-develop-iymj66chvq-uc.a.run.app/"
     language = "Español"  # Options: "Español", "en_US" , pt_BR
 
-    Email = "zenmonk-developer-zenmonk@zenmonk.tech"   # zenmonk-developer-zenmonk@zenmonk.tech
-    Password = "a2fiEz5Sx2LzRiJ"    # "a2fiEz5Sx2LzRiJ"
+    Email = "user@gmail.com"  
+    Password = "password"    
